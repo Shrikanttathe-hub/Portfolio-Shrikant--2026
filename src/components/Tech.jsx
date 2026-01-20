cdac802aa4fd748b96fcb95bd1bde79c774974e1
unknown <shrikantepitoza@gmail.com>
2026-01-20 13:29:43 +0530

    new added

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,11 +4,16 @@ import { SectionWrapper } from '../hoc';
 import { technologies } from '../constants';
 
 const Tech = () => {
+  const isMobile = window.innerWidth < 768;
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28 flex items-center justify-center' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+          {isMobile ? (
+            <img src={technology.icon} alt='icon' className="w-16 h-16 object-contain"/>
+          ) : (
+            <BallCanvas icon={technology.icon} />
+          )}
         </div>
       ))}
     </div>
