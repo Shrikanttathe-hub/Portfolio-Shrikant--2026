bc37ba9413c9d16bc094cf18c1d12528b324b5c8
unknown <shrikantepitoza@gmail.com>
2026-01-20 13:56:33 +0530

    final

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -59,21 +59,21 @@ const Contact = () => {
           <span className='text-white font-medium mb-4'>Your Name</span>
           <input type='text' name='name' value={form.name} onChange={handleChange} placeholder=" What's your name ? " 
           className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-          />
+          required/>
         </label>
 
         <label className='flex flex-col'>
           <span className='text-white font-medium mb-4'>Your Email</span>
           <input type='email' name='email' value={form.email} onChange={handleChange} placeholder=" What's your Email ? " 
           className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-          />
+          required/>
         </label>
 
         <label className='flex flex-col'>
           <span className='text-white font-medium mb-4'>Your Message</span>
           <textarea rows="7" name='message' value={form.message} onChange={handleChange} placeholder=" What do you want to say?" 
           className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-          />
+          required/>
         </label>
 
         <button type='submit' className='bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary'>{loading ? 'Sending...' : 'Send'}</button>
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BallCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { technologies } from '../constants';
+import { easeInOut, motion} from 'framer-motion';
 
 const Tech = () => {
   const isMobile = window.innerWidth < 768;
@@ -10,7 +11,15 @@ const Tech = () => {
       {technologies.map((technology) => (
         <div className='w-28 h-28 flex items-center justify-center' key={technology.name}>
           {isMobile ? (
-            <img src={technology.icon} alt='icon' className="w-16 h-16 object-contain"/>
+            <motion.img 
+            animate={{ y:[0, 25 , 0]}}
+            transition={{
+              duration: 3.4,
+              repeat: Infinity,
+              repeatType: "loop",
+              transition: easeInOut
+            }}
+            src={technology.icon} alt='icon' className="w-16 h-16 object-contain mobileTechIcon" key={technology.name}/>
           ) : (
             <BallCanvas icon={technology.icon} />
           )}
