817280a6a4383a05cc7cfa4287aa19dcdf3f879f
unknown <shrikantepitoza@gmail.com>
2026-01-20 13:18:05 +0530

    new

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -10,10 +10,10 @@ const Ball = (props) => {
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.2}>
+      <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]}/>
         <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
-        <Decal position={[0, 0, 1]} rotation={[ 2 * Math.PI, 0, 6.25]} map={decal} flatShading /> 
+        <Decal position={[0, 0, 1]}   rotation={[0, 0, 0]} scale={1} map={decal} flatShading /> 
       </mesh>
     </Float>
   )
@@ -21,10 +21,8 @@ const Ball = (props) => {
 
 const BallCanvas = ({ icon }) => {
   return (
-    <Canvas frameloop="always" dpr={[1,2]} camera={{position: [0,0,5], fov:50}} gl={{ preserveDrawingBuffer: true }} style={{width: "100%", height:"100%"}}>
+    <Canvas frameloop="always" gl={{ preserveDrawingBuffer: true }}>
     <Suspense fallback={<CanvasLoader/>}>
-     <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 5]} />
     <OrbitControls enableZoom={false} />
     <Ball imgUrl={icon} />
     </Suspense>
